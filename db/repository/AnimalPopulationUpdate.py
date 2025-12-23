44706ff88d38a92fbd6218ef185b2a177fc858e0
Nick <nkinder1@hotmail.com>
2025-12-22 19:20:08 -0800

    Added AnimalPopulationUpdate.py model and some repository functions. get_by_id, get_all, get_by_name.

---
+++ db/repository/AnimalPopulationUpdate.py
@@ -0,0 +1,54 @@
+from sqlite3 import Connection,Row
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import Optional
+
+
+@dataclass
+class AnimalPopulationUpdate:
+    id: int
+    animal_type_id: int
+    delta: int
+    date_updated: date
+    reason: Optional[str]
+
+
+def _row_to_animal_population_update(row: Row) -> AnimalPopulationUpdate:
+    return AnimalPopulationUpdate(
+        id=row['id'],
+        animal_type_id=row['animal_type_id'],
+        delta=row['delta'],
+        date_updated=datetime.strptime(row['date_updated'], '%Y-%m-%d').date(),
+        reason=row['reason']
+    )
+
+
+def get_by_id(conn: Connection, id: int) -> Optional[AnimalPopulationUpdate]:
+    result = conn.execute('SELECT * FROM animal_population WHERE id = ?', (id,)).fetchone()
+    if result:
+        return _row_to_animal_population_update(result)
+    else:
+        return None
+
+
+def get_all(conn: Connection) -> list[AnimalPopulationUpdate]:
+    result = []
+    rows = conn.execute('SELECT * FROM animal_population ORDER BY id').fetchall()
+    for row in rows:
+        result.append(_row_to_animal_population_update(row))
+    return result
+
+
+def get_by_name(conn: Connection, name: str) -> list[AnimalPopulationUpdate]:
+    sql = '''
+        SELECT apu.* 
+        FROM animal_population_update apu
+        JOIN animal_type at
+            ON apu.animal_type_id = at.animal_type_id
+        WHERE at.name = ?
+    '''
+    rows = conn.execute(sql, (name,)).fetchall()
+    result = []
+    for row in rows:
+        result.append(_row_to_animal_population_update(row))
+    return result
